a92340647e3ddc5b293b63947802748e28d75c36
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-02-19 07:17:51 +0700

    Update sistemParkir.py

--- sistemParkir.py
+++ sistemParkir.py
@@ -1,27 +1,26 @@
-#from admin import*
-#from user import*
+from admin import*
+from user import*
 import os
 import sys
 
-lagi=False
-
-while not lagi:
-    while True:
-        try:
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("Selamat datang di aplikasi Parkir Politeknnik Negeri Bandung")
-            print("\nLog in sebagai\n1.Admin\n2.User\n3.Keluar")
-            pilihan=int(input("Masukkan pilihan(1/2/3):"))
-            if pilihan == 1:
-                #admin()
-                print("Ini adalah menu admin")
-            elif pilihan == 2:
-                #user()
-                print("Ini adalah menu user")
-            elif pilihan == 3:
-                lagi = True
-                sys.exit("Keluar paksa dari program")
-            else:
-                print("Pilihan tidak valid, pilih 1 untuk Ya atau 2 untuk Tidak.")  # Menangani input selain 1 atau 2
-        except ValueError:
-            print("Input tidak valid! Harap masukkan angka 1 atau 2.")  # Menangani input selain angka
+def main():
+    lagi=False
+    while not lagi:
+        while True:
+            try:
+                os.system('cls')
+                print("Selamat datang di aplikasi Parkir Politeknnik Negeri Bandung")
+                print("\nLog in sebagai\n1.Admin\n2.User\n3.Keluar")
+                pilihan=int(input("Masukkan pilihan(1/2/3):"))
+                if pilihan == 1:
+                    admin()
+                elif pilihan == 2:
+                    user()
+                elif pilihan == 3:
+                    lagi = True
+                    sys.exit("Keluar paksa dari program")
+                else:
+                    print("Pilihan tidak valid, pilih 1 untuk Ya atau 2 untuk Tidak.")  # Menangani input selain 1 atau 2
+            except ValueError:
+                print("Input tidak valid! Harap masukkan angka 1 atau 2.")  # Menangani input selain angka
+        
