78cde6831f82e7b9fa2366b09d768f0869ab20ed
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-02-19 07:15:59 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,24 @@
-def riwayat():
-    file = open("riwayat.txt", "r")
-    b = file.read().splitlines()  
-    b.sort()  
-    print(b)
+from modulAdmin import*
+import os
+
+def admin():
+    while True:
+        os.system('cls')
+        print("#=================================#")
+        print("||          LAMAN ADMIN          ||")
+        print("#=================================#")
+        print("|| 1. Lihat Riwayat Parkir       ||")
+        print("|| 2. Keluar                     ||")
+        print("#=================================#")
+
+        pilihan = input("Silahkan pilih satu pilihan: ")
+
+        if pilihan == '1':
+            riwayat()
+        
+        elif pilihan == '2':
+            print("Keluar dari sistem parkir.")
+            break
+
+        else:
+            print("Input tidak valid. Pilih 1 atau 2.")
