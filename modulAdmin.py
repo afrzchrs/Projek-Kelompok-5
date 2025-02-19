077c21f98d0a0a58899fbd418cc11f713f2a83d7
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-02-19 17:44:46 +0700

    Update modulAdmin.py

--- modulAdmin.py
+++ modulAdmin.py
@@ -1,5 +1,12 @@
+import os
 def riwayat():
-    file = open("riwayat.txt", "r")
-    b = file.read().splitlines()  
-    b.sort()  
-    print(b)
+    os.system('cls')
+    with open("riwayat.txt", "r") as file:
+                b = file.read().splitlines()
+                b.sort()
+                for line in b:
+                    print(line)
+    
+    kembali = input("\n\nkembali?:(Y/y)").lower()
+    if kembali == 'y':
+        return
